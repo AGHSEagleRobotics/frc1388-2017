d12014c160a334d8422d4d40ff4b9801aaedabda
Joseph Kraut <joseph.h.kraut@gmail.com>
2017-01-14 14:44:19 -0800

    Added a class for vision and camera operations.

---
+++ src/Commands/Vision.h
@@ -0,0 +1,20 @@
+/*
+ * Vision.h
+ *
+ *  Created on: Jan 14, 2017
+ *      Author: Joey
+ */
+
+#ifndef SRC_COMMANDS_VISION_H_
+#define SRC_COMMANDS_VISION_H_
+#include <string>
+
+using namespace std;
+
+class Vision {
+public:
+	Vision(string ip);
+	virtual ~Vision();
+};
+
+#endif /* SRC_COMMANDS_VISION_H_ */
