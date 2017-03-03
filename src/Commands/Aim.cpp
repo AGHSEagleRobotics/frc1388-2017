c29a5515cf42a26bdae55ae846897b2bd71f0768
Phillip Kravtsov <phillip@kravtsov.net>
2017-03-02 18:31:30 -0800

    Probably stop build day code hopefully

--- src/Commands/Aim.cpp
+++ src/Commands/Aim.cpp
@@ -39,14 +39,15 @@ void Aim::Initialize() {
 
 // Called repeatedly when this Command is scheduled to run
 void Aim::Execute() {
-
-	double z = Robot::oi->getOpStick()->GetTwist();
-	if ((RobotMap::aimerBottomLimit && z < 0) || (RobotMap::aimerTopLimit && z > 0)){
-		RobotMap::aimerAimMotor->Set(0.0);
-	}
-	else{
-		RobotMap::aimerAimMotor->Set(Deadband(z));
-	}
+//
+//	double z = Robot::oi->getOpStick()->GetTwist();
+//
+//	if ((RobotMap::aimerBottomLimit && z < 0) || (RobotMap::aimerTopLimit && z > 0)){
+//		RobotMap::aimerAimMotor->Set(0.0);
+//	}
+//	else{
+//		RobotMap::aimerAimMotor->Set(Deadband(z));
+//	}
 
 }
 
